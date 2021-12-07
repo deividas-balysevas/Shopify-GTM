2e26ac48fa7eff910b451dcb1e3b966f2d4a7629
thekWzON <34367567+thekWzON@users.noreply.github.com>
2021-12-07 20:11:12 +0200

    Update shopify.js
    Fixed typo.

--- shopify.js
+++ shopify.js
@@ -5,6 +5,6 @@
         'total_price': {{ order.total_price | money_without_currency }},
         'currency' : {{ currency.iso_code }},
 	'customerEmail' : {{ order.email }},
-	'customerPhone' : {{ prder.phone }}
+	'customerPhone' : {{ order.phone }}
     });
 </script> 
