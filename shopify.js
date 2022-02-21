9caa48d3aa5bfcff74aa36ab3a1c5423ae147516
Deividas <34367567+deividas-balysevas@users.noreply.github.com>
2022-02-21 12:45:08 +0200

    Update shopify.js

--- shopify.js
+++ shopify.js
@@ -3,8 +3,7 @@
         'event': 'Transaction',
         'order_number': {{ order.order_number }},
         'total_price': {{ order.total_price | money_without_currency }},
-        'currency' : {{ currency.iso_code }},
-	'customerEmail' : {{ order.email }},
-	'customerPhone' : {{ order.phone }}
+	'customerEmail' : '{{ order.email }}',
+	'customerPhone' : '{{ order.phone }}'
     });
 </script> 
