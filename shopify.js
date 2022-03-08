7b5b26327dba989749a384881f718b4636fdbbb9
Deividas <34367567+deividas-balysevas@users.noreply.github.com>
2022-03-08 11:23:14 +0200

    Update shopify.js

--- shopify.js
+++ shopify.js
@@ -1,9 +1,23 @@
+{% if first_time_accessed %}
 <script>
-    dataLayer.push({
-        'event': 'Transaction',
-        'order_number': {{ order.order_number }},
-        'total_price': {{ order.total_price | money_without_currency }},
-	'customerEmail' : '{{ order.email }}',
-	'customerPhone' : '{{ order.phone }}'
-    });
-</script> 
+  var ecData = {
+    "first_name": "{{ checkout.billing_address.first_name }}",
+    "last_name": "{{ checkout.billing_address.last_name }}",
+    "home_address": {
+      "street": "{{ checkout.billing_address.street }}",
+      "city": "{{ checkout.billing_address.city }}",
+      "region": "{{ checkout.billing_address.province }}",
+      "postal_code": "{{ checkout.billing_address.zip }}",
+      "country": "{{ checkout.billing_address.country_code }}"
+    }
+  }
+if("{{ checkout.email }}"){
+   ecData.email = "{{ checkout.email }}";
+}
+if("{{ checkout.billing_address.phone }}"){
+   ecData.phone_number = "{{ checkout.billing_address.phone }}";
+}
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({'event':'orderComplete', 'orderValue': {{ checkout.total_price | divided_by: 100.0 }}, 'orderId': '{{ order_number }}', 'orderCurrency': '{{ currency }}', 'ecData': ecData});
+</script>
+{% endif %}
